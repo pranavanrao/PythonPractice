3a75d1920647d64f4bfb9b881bfb7b9a80cc7142
Pranav Rao <pranavanrao@gmail.com>
2024-05-22 15:53:29 +0530

    Add bubble sort algorithm

---
+++ bubbleSort.py
@@ -0,0 +1,27 @@
+def swapItems(a, i, j) :
+    temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
+
+def bubbleSort(a, size) :
+    for i in range(size) :
+        for j in range(i+1, size) :
+            if (a[j] < a[i]) :
+                swapItems(a, i, j)
+        print(a[i], end=" ")
+
+size = int(input("Enter the size of an array : "))
+
+a = []
+
+for i in range(size) :
+    element = input(f"Enter the array item for index {i}: ")
+    a.append(element)
+
+print("The unsorted array : ")
+for i in range(size) :
+    print(a[i], end=" ")
+
+print("\nThe sorted array :")
+
+bubbleSort(a,size)
